22a60f0e07433d7264c2f807362eea70ea2350a8
ogunnowoomotayo <ogunnowoomotayo14@gmail.com>
2024-05-02 20:44:57 +0100

    my first commit msg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState   } from "react";
 
 function App() {
+  //STATE FOR THE TODOS
+  const[todolist, setTodoList] = useState([]);
+
+  //state for the input box
+  const [inputText, setInputText] = useState("");
+
+  //add todo function
+  const addTodo = () => {
+    if(!inputText){
+      console.log("Please enter a text");
+      return false;
+    }
+    let newTodo = {
+      id: Date.now(),
+      name: inputText,
+    };
+
+    setTodoList([...todolist, newTodo]);
+    setInputText("");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div
+      style = {{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        gap: 10,
+      }}
+      >
+        <input
+        placeholder="add todo"
+        style={{
+          border: "1px solid red",
+          borderRadius: "5px",
+        }}
+        value={inputText}
+        onChange={(value)  => {
+          setInputText(value.target.value);
+        }}
+        />
+        <button onclick = {addTodo}>Add tode</button>
+      </div>
+      <div>
+        <p>ALL Todo list</p>
+        {todoList?.map((text)  =>  {
+          return (
+            <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              gap: 10,
+            }}
+            key={text.id}
+            >
+              <p>{text.name}</p>
+              <span
+                onClick={() => removeTodo(text.id)}
+                style={{color: "red" }}
+              >
+                delete
+              </span>
+            </div>
+          );
+        })}
+      </div>
     </div>
+
   );
 }
 
